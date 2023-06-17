9ad040f4751fa67ac2b8dbd0c91adfd7a2ebb34c
Putmonyrath-Vann <pvann2@paragoniu.edu.kh>
2023-06-17 17:52:38 +0700

    nav bar changes

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -5,6 +5,8 @@
     <div class="nav-right">
         <a href="/customers">Customer</a>
         <a href="/drivers">Driver</a>
+        <a href="/signup/customer">Customer Sign Up</a>
+        <a href="/signup/driver">Driver Sign Up</a>
         <form action="/logout" method="POST">
             @csrf
             <button type="submit">Log out</button>
