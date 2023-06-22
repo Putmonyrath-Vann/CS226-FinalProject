68b0714bdccc357971c88ff5f16b0a7207d2896a
Putmonyrath-Vann <pvann2@paragoniu.edu.kh>
2023-06-22 11:21:54 +0700

    updated css for food to not stuck on each other

--- resources/views/restaurant/food.blade.php
+++ resources/views/restaurant/food.blade.php
@@ -12,12 +12,14 @@
             <span class="error">{{ $errors->first('food_image.' . $i) }}</span>
         @endif
     @endfor
-        <div class="alert alert-danger">
-            @if ($errors->has('food_image'))
-                <script>alert({{"file size too big"}})</script>
-            @endif
+    <div class="alert alert-danger">
+        @if ($errors->has('food_image'))
+            <script>
+                alert({{ 'file size too big' }})
+            </script>
+        @endif
 
-        </div>
+    </div>
 
     @if ($foods->isEmpty())
         <h1>Your shop haven't added any food yet</h1>
@@ -26,19 +28,19 @@
         </div>
 
         <div id="buttons">
-            <button class="category-food-add-btn" onclick="addFood({{$categories}})">add new food</button>
+            <button class="category-food-add-btn" onclick="addFood({{ $categories }})">add new food</button>
         </div>
     @else
         {{-- {{$categories}} --}}
         <h1>Your shop's foods</h1>
-            <div style="width: 70%;">
-                @foreach($foods as $food)
-                    <div class="food">
-                        <div class="image-for-food">
-                            <img src="{{$food->img}}" alt="food" class="food-image"/>
-                        </div>
-                        <div class="food-detail">
-                        <p class="food-name">{{$food->name}}</p>
+        <div style="width: 70%;">
+            @foreach ($foods as $food)
+                <div class="food">
+                    <div class="image-for-food">
+                        <img src="{{ $food->img }}" alt="food" class="food-image" />
+                    </div>
+                    <div class="food-detail">
+                        <p class="food-name">{{ $food->name }}</p>
                         <p class="food-secondary-text">
                             @php
                                 foreach ($categories as $category) {
@@ -52,23 +54,24 @@
                             @php
                                 $price = $food->price;
                                 $price = number_format($price, 2, '.', ',');
-                                echo '$'.$price;
+                                echo '$' . $price;
                             @endphp
                         </p>
-                        <a href="/restaurant/delete/food/{{$food->food_id}}">
-                            <img src="/trash-bin.png" alt="delete" class="delete-icon self-end"/>
-                        </a>
+
                     </div>
-                @endforeach
-                <form action="/restaurant/add/food" method="post" id="form">@csrf</form>
-            </div>
+                    <a href="/restaurant/delete/food/{{ $food->food_id }}">
+                        <img src="/trash-bin.png" alt="delete" class="delete-icon self-end" />
+                    </a>
+                </div>
+            @endforeach
+            <form action="/restaurant/add/food" method="post" id="form">@csrf</form>
         </div>
 
-            <div id="buttons">
-                <button class="category-food-add-btn" onclick="addFood($categories)">add new food</button>
-            </div>
+        <div id="buttons">
+            <button class="category-food-add-btn" onclick="addFood($categories)">add new food</button>
+        </div>
 
-            {{-- <form action="/restaurant/add/category" method="post" id="form">
+        {{-- <form action="/restaurant/add/category" method="post" id="form">
                 @csrf
                 <input type="text" name="category_name" class="card" id="add" placeholder="Add New Category"/>
             </form>
